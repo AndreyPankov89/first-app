2ae5bc461ade2725eb05c03b1cbefe3c41b784fa
hjicvi <apan89@mail.ru>
2019-01-21 17:30:21 +0500

    lesson 3.1

--- first-app/src/components/post-list-item/post-list-item.js
+++ first-app/src/components/post-list-item/post-list-item.js
@@ -3,11 +3,20 @@ import React from 'react';
 import './post-list-item.css';
 
 const PostListItem = () => {
+    const today = new Date(); 
+    const tekDate =`${today.getDate()}/${today.getMonth()+1}/${today.getFullYear()}`;
+    
     return (
         <li className="app-list-item d-flex justify-content-between">
-            <span className="app-list-item-label">
-                Lorem ipsum dolor sit amet.
+            <span className="label-wrapper">
+                <span className="app-list-item-label">
+                    Lorem ipsum dolor sit amet.
+                </span>
+                <span className="app-list-item-data">
+                    {tekDate}
+                </span>
             </span>
+            
             <div className="d-flex justify-content-center align-items-center">
                 <button type="button" className="btn-star btn-small">
                     <i className="fa fa-star"></i>
@@ -17,6 +26,7 @@ const PostListItem = () => {
                 </button>   
                     <i className="fa fa-heart"></i>
             </div>
+
         </li>
     )
 }
